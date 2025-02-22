aa7b010ae68bf4d0c1e28e71acd8e63f503b6ec7
kangjoonhyeok <centerhs12@pusan.ac.kr>
2025-02-22 18:44:15 +0900

    13

--- 13/13.js
+++ 13/13.js
@@ -2,7 +2,7 @@
 const mvApi = 'b2de8d2086e4712724ce826185cd9d21';
 //영화진흥위원회
 const baseMvUrl = `http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?`;
-
+                    
 //TMDB
 const rmdbApi = '063d383d56921ac223f1e3a619c101f3';
 const baseTMDBUrl = 'https://api.themoviedb.org/3/search/movie?';
@@ -10,37 +10,6 @@ const baseTMDBUrl = 'https://api.themoviedb.org/3/search/movie?';
 //영화포스터 저장 배열
 let posterArr = [];
 
-const BoxofficeList() =>
-
-const getFetch = async (dt, ul) => {
-    let apiKey = 'b2de8d2086e4712724ce826185cd9d21';
-    let baseUrl = 'http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?';
-    let url = `${baseUrl}key=${apiKey}&targetDt=${dt}`;
-    const mvType = document.querySelector('[type=radio]:checked').value;
-    //
-    try {
-        if(mvType == 'k'){
-            url += `&repNationCd=${mvType}`;
-
-        }
-        const rasp = await fetch(url);
-        const data = await rasp.json();
-        let tags = '';
-        for (let item of data.boxOfficeResult.dailyBoxOfficeList) {
-            tags = tags +
-                `<li class="fetchLi">
-                <span class="sp">${item.rank}</span>
-                ${item.movieNm}
-            </li>`;
-        }
-        ul.innerHTML = tags;
-        console.log(data.boxOfficeResult.dailyBoxOfficeList)
-    } catch (err) {
-
-        console.log(err);
-    }
-    console.log(dt);
-}
 
 //포스터 가져오기
 const getPoster = async (movieNm) => {
@@ -58,17 +27,19 @@ const getPoster = async (movieNm) => {
 
 //박스오피스 가져오기
 const getBoxOfficeList = async () => {
+    const ul = document.querySelector('ul');
+    const mvPoster = document.querySelector('#mvPost');
     const mvType = document.querySelector('[type=radio]:checked').value;
     const dt = document.querySelector('[type=date]').value.replaceAll('-', '');
-    console.log("mvType", mvType, dt)
+    console.log("mvType", mvType, dt);
 
     //포스터 배열 초기화
     posterArr = [];
     let url = `${baseMvUrl}&key=${mvApi}&targetDt=${dt}`;
-    if (mvType == 'K' || mvType == 'F') {
+    if(mvType == 'K' || mvType == 'F'){
         url = `${url}&repNationCd=${mvType}`;
     }
-
+    console.log(url);
     //fetch
     const resp = await fetch(url);
     const data = await resp.json();
@@ -76,11 +47,25 @@ const getBoxOfficeList = async () => {
 
     let tags = '';
     for (let boxoffice of dailyBoxOfficeList) {
-
+        tags = tags + 
+          `<li>
+            <span class="sp">${boxoffice.rank}</span>
+            ${boxoffice.movieNm}
+          </li>` ;
         await getPoster(boxoffice.movieNm);
     }
 
+    ul.innerHTML = tags;
 
+    const lis = document.querySelectorAll('li');
+    for(let [i, li] of lis.entries()){
+        li.addEventListener('mouseover', () => {
+            posterArr[i] == ''
+            ? mvPoster.innerHTML = "자료없음"
+            : mvPoster.innerHTML =  `<img src="https://image.tmdb.org/t/p/w500${posterArr[i]}">`;
+        })
+    }
+    
     console.log("posterArr", posterArr);
 }
 
@@ -106,9 +91,7 @@ const getYesterday = () => {
 
 document.addEventListener('DOMContentLoaded', () => {
     const inputDate = document.querySelector('[type=date]');
-    const radios = document.querySelectorAll('[type=radio]');
     const bt = document.querySelector('button');
-    const ul = document.querySelector('ul');
 
     //어제 날짜 
     const yesterday = getYesterday();
@@ -123,8 +106,6 @@ document.addEventListener('DOMContentLoaded', () => {
         e.preventDefault();
 
         getBoxOfficeList();
-        getFetch(inputDate.value.replaceAll('-', ''), ul);
-        console.log(radios[0].value);
 
   });
 
